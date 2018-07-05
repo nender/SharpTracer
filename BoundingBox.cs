72a6ad59ffc39f174303c70cc751fbe60be9544f
nender <nathan.ender@gmail.com>
2018-07-05 13:30:25 +0200

    Added ray-box intersection

--- BoundingBox.cs
+++ BoundingBox.cs
@@ -5,30 +5,30 @@ using System.Collections.Generic;
 namespace RayTracer {
     /// Axis aligned bounding box
     class BoundingBox {
-        Vec3 P1;
-        Vec3 P2;
         public double Width, Height, Depth;
         public Vec3 Midpoint;
 
-        public BoundingBox(Vec3 p1, Vec3 p2) {
-            if (p1.X <= p2.X || p1.Y <= p2.Y || p1.Z <= p2.Z)
+        Vec3 Max;
+        Vec3 Min;
+
+        public BoundingBox(Vec3 min, Vec3 max) {
+            if (max.X <= min.X || max.Y <= min.Y || max.Z <= min.Z)
                 throw new ArgumentException("P2 is greater than P1!");
-            P1 = p1;
-            P2 = p2;
+            Max = max;
+            Min = min;
         }
 
         public BoundingBox(Vec3 center, double radius) {
-            P1 = center + new Vec3(radius);
-            P2 = center - new Vec3(radius);
+            Max = center + new Vec3(radius);
+            Min = center - new Vec3(radius);
         }
 
         public BoundingBox(IEnumerable<IHitable> objects) {
             const double inf = double.PositiveInfinity;
-            // bounds is a concatenation of the two points
             var bounds = new double[6] {-inf, -inf, -inf, inf, inf, inf};
             foreach (var o in objects) {
                 var box = o.BoundingBox();
-                var ibounds = box.P1.Concat(box.P2).ToArray();
+                var ibounds = box.Max.Concat(box.Min).ToArray();
                 for (int i = 0; i < 3; i++)
                     if (ibounds[i] > bounds[i])
                         bounds[i] = ibounds[i];
@@ -37,16 +37,36 @@ namespace RayTracer {
                         bounds[i] = ibounds[i];
             }
 
-            P1 = new Vec3(bounds[0], bounds[1], bounds[2]);
-            P2 = new Vec3(bounds[3], bounds[4], bounds[5]);
+            Max = new Vec3(bounds[0], bounds[1], bounds[2]);
+            Min = new Vec3(bounds[3], bounds[4], bounds[5]);
         }
 
         public bool Intersect(BoundingBox other) {
             throw new NotImplementedException();
         }
 
-        public bool Intersect(Ray ray) {
-            throw new NotImplementedException();
+        public bool Intersect(Ray r) {
+            Vec3 tvmin = (Min - r.Origin) / r.Direction;
+            Vec3 tvmax = (Max - r.Origin) / r.Direction;
+
+            double tmin = Math.Min(tvmin.X, tvmax.X);
+            double tmax = Math.Max(tvmin.X, tvmax.X);
+            double tymin = Math.Min(tvmin.Y, tvmax.Y);
+            double tymax = Math.Max(tvmin.Y, tvmax.Y);
+
+            if ((tmin > tymax) || (tymin > tmax))
+                return false;
+
+            tmin = Math.Min(tymin, tmin);
+            tmax = Math.Max(tymax, tmax);
+
+            double tzmin = Math.Min(tvmin.Z, tvmax.Z);
+            double tzmax = Math.Max(tvmin.Z, tvmax.Z);
+
+            if ((tmin > tzmax) || (tzmin > tmax))
+                return false;
+            
+            return true;
         }
     }
 }
