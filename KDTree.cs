534d2d14dff980932923e9885a7960dc10a0cba7
nender <nathan.ender@gmail.com>
2018-07-05 13:53:08 +0200

    Added method to walk KDTree for hits

--- KDTree.cs
+++ KDTree.cs
@@ -3,8 +3,7 @@ using System.Linq;
 using System.Collections.Generic;
 
 namespace RayTracer {
-    class KDTree : IHitable
-    {
+    class KDTree : IHitable {
         KDNode Root;
 
         public KDTree(IEnumerable<IHitable> objects) {
@@ -30,8 +29,7 @@ namespace RayTracer {
         KDNode LeftChild;
         KDNode RightChild;
 
-        public KDNode(IEnumerable<IHitable> objects)
-        {
+        public KDNode(IEnumerable<IHitable> objects) {
             BBox = new BoundingBox(objects);
             var left = new List<IHitable>();
             var right = new List<IHitable>();
@@ -70,9 +68,30 @@ namespace RayTracer {
             .First();
         }
 
-        public HitRecord? Hit(Ray r, double tMin, double tMax)
-        {
-            throw new NotImplementedException();
+        public HitRecord? Hit(Ray r, double tMin, double tMax) {
+            if (BBox.Intersect(r)) {
+                if (LeftChild != null || RightChild != null) {
+                    var leftHit = LeftChild?.Hit(r, tMin, tMax);
+                    var rightHit = RightChild?.Hit(r, tMin, tMax);
+                    if (leftHit?.t <= rightHit?.t)
+                        return leftHit;
+                    else if (rightHit?.t > leftHit?.t)
+                        return rightHit;
+                    else
+                        return null;
+                } else {
+                    HitRecord? bestHit = null;
+                    foreach (var o in objects) {
+                        var hit = o.Hit(r, tMin, tMax);
+                        if (hit?.t < bestHit?.t || hit != null && bestHit == null)
+                            bestHit = hit;
+                    }
+                    return bestHit;
+                }
+            } else {
+                return null;
+            }
         }
+
     }
 }
